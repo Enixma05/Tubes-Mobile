7227146e0294cf243d910c1a794bdf38a65ddf26
syucii <sucitsrirhmdni.10@gmail.com>
2026-05-12 12:54:14 +0800

    Frontend HOME

---
+++ app/src/main/java/com/example/rabu/Buku.kt
@@ -0,0 +1,7 @@
+package com.example.rabu
+
+data class Buku(
+    val judul: String,
+    val deskripsi: String,
+    val progress: Int
+)
---
+++ app/src/main/java/com/example/rabu/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.rabu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerBuku: RecyclerView
+    private lateinit var bukuAdapter: BukuAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        recyclerBuku = findViewById(R.id.recyclerBuku)
+
+        val listBuku = listOf(
+            Buku(
+                "Atomic Habits",
+                "Buku tentang membangun kebiasaan kecil.",
+                70
+            ),
+            Buku(
+                "Laut Bercerita",
+                "Novel karya Leila S. Chudori.",
+                45
+            )
+        )
+
+        bukuAdapter = BukuAdapter(listBuku)
+
+        recyclerBuku.layoutManager = LinearLayoutManager(this)
+        recyclerBuku.adapter = bukuAdapter
+    }
+}
